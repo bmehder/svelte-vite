56fe12aad9d3758592f4b015f20af83de7ff4b5b
bmehder <bmehder@gmail.com>
2022-08-31 19:02:05 +0300

    added plugin to change style bundle name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    {
+      name: 'custom-css-name',
+      enforce: 'post',
+      generateBundle(outputOptions, bundle) {
+        bundle['style.css'].fileName = 'bundle.css'
+        bundle['bundle.css'] = bundle['style.css']
+        delete bundle['style.css']
+      },
+    },
+  ],
   build: {
     lib: {
       entry: 'src/main.js',
